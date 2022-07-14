26a8a8233ceaa7fedd1523dd62e2e69f5c78121e
Kenert Karu <karukenert@gmail.com>
2022-07-15 00:16:14 +0300

    use tailwind with signin form

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,58 +1,175 @@
 <template>
-    <h1>Sign in </h1>  
-    <p><input type="text" placeholder="Email" v-model="email"/></p>
-    <p><input type="password" placeholder="Password" v-model="password"/></p>
-    <p v-if="errMsg">{{errMsg}}</p>
-    <p><button @click="logIn">Log in</button></p>
-    <p><button @click="singInWithGoogle"> Sign in with Google</button></p>
+  <h1>Sign in</h1>
+  <div
+    class="
+      min-h-full
+      flex
+      items-center
+      justify-center
+      py-12
+      px-4
+      sm:px-6
+      lg:px-8
+    "
+  >
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <div class="rounded-md shadow-sm -space-y-px">
+        <div>
+          <label for="email-address" class="sr-only">Email address</label>
+          <input
+            v-model="email"
+            id="email-address"
+            name="email"
+            type="email"
+            autocomplete="email"
+            class="
+              appearance-none
+              rounded-none
+              relative
+              block
+              w-full
+              px-3
+              py-2
+              border border-gray-300
+              placeholder-gray-500
+              text-gray-900
+              rounded-t-md
+              focus:outline-none
+              focus:ring-indigo-500
+              focus:border-indigo-500
+              focus:z-10
+              sm:text-sm
+            "
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <label for="password" class="sr-only">Password</label>
+          <input
+            v-model="password"
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            class="
+              appearance-none
+              rounded-none
+              relative
+              block
+              w-full
+              px-3
+              py-2
+              border border-gray-300
+              placeholder-gray-500
+              text-gray-900
+              rounded-b-md
+              focus:outline-none
+              focus:ring-indigo-500
+              focus:border-indigo-500
+              focus:z-10
+              sm:text-sm
+            "
+            placeholder="Password"
+          />
+        </div>
+      </div>
+      <p class="" v-if="errMsg">{{ errMsg }}</p>
+
+      <div>
+        <button
+          @click="logIn"
+          class="
+            group
+            relative
+            w-full
+            flex
+            justify-center
+            py-2
+            px-4
+            border border-transparent
+            text-sm
+            font-medium
+            rounded-md
+            text-white
+            bg-indigo-600
+            hover:bg-indigo-700
+            focus:outline-none
+            focus:ring-2
+            focus:ring-offset-2
+            focus:ring-indigo-500
+          "
+        >
+          <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+            <LockClosedIcon
+              class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+              aria-hidden="true"
+            />
+          </span>
+          Sign in
+        </button>
+
+        Or
+        <br />
+        <span @click="singInWithGoogle">Sign in with Google!</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { useRouter } from "vue-router";
 
-const router = useRouter()
+const router = useRouter();
 
-const email = ref('')
-const password = ref('')
-const errMsg = ref('')
+const email = ref("");
+const password = ref("");
+const errMsg = ref("");
 
-const logIn = async () =>{
-    const auth = getAuth()
-    try{
-     await signInWithEmailAndPassword(auth, email.value, password.value);
-    router.push('/feed');
-    }catch (error){
-        switch(error.code){
-            case 'auth/invalid-email':
-            errMsg.value = 'Vigane email';
-            break;
+const logIn = async () => {
+  const auth = getAuth();
+  try {
+    await signInWithEmailAndPassword(auth, email.value, password.value);
+    router.push("/feed");
+  } catch (error) {
+    switch (error.code) {
+      case "auth/invalid-email":
+        errMsg.value = "Vigane email";
+        break;
 
-            case 'auth/user-not-found':
-            errMsg.value = 'Kasutajat ei leitud';
-            break;
+      case "auth/user-not-found":
+        errMsg.value = "Kasutajat ei leitud";
+        break;
 
-            case 'auth/wrong-password':
-            errMsg.value = 'Vale parool';
-            break;
+      case "auth/wrong-password":
+        errMsg.value = "Vale parool";
+        break;
 
-            default:
-            errMsg.value = 'Vale email või parool';
-            break;
-            
-        }
+      default:
+        errMsg.value = "Vale email või parool";
+        break;
     }
+  }
+};
 
-}
-
-const singInWithGoogle = ()=>{
-    const provider = new GoogleAuthProvider()
-    signInWithPopup(getAuth(), provider)
-    .then((result)=> {
-            console.log(result.user)
-            router.push('/feed');
-        })
-    .catch((e)=>console.error(e))
-}
+const singInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log(result.user);
+      router.push("/feed");
+    })
+    .catch((e) => console.error(e));
+};
 </script>
