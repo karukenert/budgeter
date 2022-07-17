6831a08a55cc2c0acc7866f009292db889e4a210
Kenert Karu <karukenert@gmail.com>
2022-07-17 11:28:38 +0300

    improve scripts / serve dist on every route

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const serveStatic = require("serve-static");
 const path = require('path');
 
 const port = process.env.PORT || 8080;
+const distLocation = path.join(__dirname, '../client/dist');
 const app = express();
-app.use(serveStatic(path.join(__dirname, '../client/dist')));
-app.listen(port);
 
+// Serving vue app
+app.use(serveStatic(distLocation));
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, (req, res) => res.sendFile(`${distLocation}/index.html`));
+
+
+app.listen(port);
 console.log(`FE is served at port ${port}`)
