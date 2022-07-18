65443246cef0c5d2434c39a91772bc4dc5350f8a
Kenert Karu <karukenert@gmail.com>
2022-07-19 00:20:47 +0300

    improve scripts

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const serveStatic = require("serve-static");
 const path = require('path');
 const apiRouter = require('./routes/api')
 
-const port = process.env.PORT || 8080;
 const distLocation = path.join(__dirname, '../client/dist');
 const app = express();
 
 // Serving api routes as a middleware
 app.use('/api', apiRouter);
 
+// TODO: serve vite app and BE on the same 3000 port
+// TODO: only serve dist if NODE_ENV === 'production'
 // Serving vue app
-app.use(serveStatic(distLocation));
+app.use(express.static(distLocation));
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, (req, res) => res.sendFile(`${distLocation}/index.html`));
 
 
+const port = process.env.PORT || 3000;
 app.listen(port);
-console.log(`FE is served at port ${port}`)
+console.log(`App is served at port ${port}`)
